bec6fa2ba6800eb2e2f5f729fd4230bff94dedc9
Bridget <bridgetjpereira@gmail.com>
2022-01-04 16:18:01 +0000

    Mapping over data

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,20 @@ import React from "react";
 import Card from "./components/Card";
 import beers from "./data/beers";
 import styles from "./App.module.scss";
+import NavBar from "./components/NavBar/NavBar.jsx";
 
 const App = () => {
-  const getCardFrontJSX = (beer) => <Card beer={beer} />;
-  return <div className={styles.App}>{beers.map(getCardFrontJSX)}</div>;
+  const getCardFrontJSX = (beer) => <Card beer={beer} key={beer.id} />;
+
+  return (
+    <>
+      <div className={styles.App}>{beers.map(getCardFrontJSX)}</div>
+      <section className={styles.nav}>
+        <NavBar />;
+      </section>
+      ;
+    </>
+  );
 };
 
 export default App;
---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./NavBar.module.scss";
+
+const Navbar = () => {
+  return <div>Hello I'm here...</div>;
+};
+
+export default Navbar;
