d0a15d26a253adad707b54b752524dd1e417c176
Bridget <bridgetjpereira@gmail.com>
2022-01-26 17:50:38 +0000

    Basic unit test Card

--- src/components/Card/Card.test.js
+++ src/components/Card/Card.test.js
@@ -1,33 +1,15 @@
-import '@testing-library/jest-dom';
-import Enzyme from 'enzyme';
-import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
+import React from "react";
+import Card from "./Card";
+import { shallow } from "enzyme";
 
-Enzyme.configure({ adapter: new Adapter() });
+describe("Card tests", () => {
+  let component;
 
+  beforeEach(() => {
+    component = shallow(<Card />);
+  });
 
-
-
-// const Card = (props) => {
-//     return (
-//       <div className={styles.card}>
-//         <img src={props.beer.image_url} alt="Best Beer" />
-
-//         <div className={styles.cardTitle}>{props.beer.name}</div>
-//         <div className={styles.tagTitle}>{props.beer.tagline}</div>
-
-//         <section className={styles.beerDetails}>
-//           <p>
-//             <span className={styles.description}>{props.beer.description}</span>
-//           </p>
-//         </section>
-//       </div>
-//     );
-//   };
-
-//   export default Card;
-
-// Test: it should render beer image on the page...
-//       it should render cardtitle on the page...
-//       it should render tagline
-//       it should render beerDetails
-//       it should render description
+  it("should render", () => {
+    expect(component).toBeTruthy();
+  });
+});
