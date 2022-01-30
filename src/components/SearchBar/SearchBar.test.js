fcef0a5a98d58df0a4ebb33dc4fbc4b8c86f2efb
Bridget <bridgetjpereira@gmail.com>
2022-01-30 23:17:18 +0000

    working on unit testing

--- src/components/SearchBar/SearchBar.test.js
+++ src/components/SearchBar/SearchBar.test.js
@@ -1,14 +1,16 @@
 import React from "react";
 import SearchBar from "./SearchBar";
 import { shallow } from "enzyme";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 describe("SearchBar tests", () => {
   let component;
   let mockFn;
+  let setIsOpen;
 
   beforeEach(() => {
     mockFn = jest.fn();
-    component = shallow(<SearchBar setSearchText={mockFn} />);
+    component = shallow(<SearchBar setSearchText={mockFn} setIsOpen={false} />);
   });
 
   it("should render", () => {
@@ -30,4 +32,22 @@ describe("SearchBar tests", () => {
       .simulate("input", { target: { value: "User input" } });
     expect(mockFn).toHaveBeenCalledWith("User input");
   });
+});
+
+//Test the searchbar open and close functionality. When we click the icon,
+//the searchbar should appear or disappear.
+
+it("should not be open by default", () => {
+  const element = component.find(".fa");
+  expect(element.is("setIsOpen")).toBe(false);
+});
+
+it("should toggle faceDown class when component is clicked", () => {
+  expect(component.find(".fa").hasClass("input")).toBe(false);
+
+  component.find(".fa").simulate("click");
+  expect(component.find(".fa").hasClass("input")).toBe(true);
+
+  component.find(".fa").simulate("click");
+  expect(component.find(".fa").hasClass("input")).toBe(false);
 });
